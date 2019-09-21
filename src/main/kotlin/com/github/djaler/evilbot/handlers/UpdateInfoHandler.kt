7b2321feeb9893c31ef6771f09c8700b68c87292
Kirill Romanov <djaler1@gmail.com>
2019-09-21 14:25:55 +0300

    improvement: отправка в sentry структурированных данных о текущем пользователе

--- src/main/kotlin/com/github/djaler/evilbot/handlers/UpdateInfoHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/UpdateInfoHandler.kt
@@ -5,21 +5,33 @@ import com.github.djaler.evilbot.filters.not
 import com.github.djaler.evilbot.service.ChatService
 import com.github.djaler.evilbot.service.UserService
 import com.github.djaler.evilbot.utils.usernameOrName
+import io.sentry.SentryClient
+import io.sentry.event.UserBuilder
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.Message
 
 @Component
 class UpdateInfoHandler(
     private val userService: UserService,
-    private val chatService: ChatService
+    private val chatService: ChatService,
+    private val sentryClient: SentryClient
 ) : MessageHandler(filter = Filters.PrivateChat.not()) {
     override val order = 0
 
     override fun handleMessage(message: Message): Boolean {
-        val user = userService.getUser(message.from.id)
+        val fromUser = message.from
+
+        sentryClient.context.user = UserBuilder()
+            .setId(fromUser.id.toString())
+            .setUsername(fromUser.usernameOrName)
+            .withData("chatTitle", message.chat.title)
+            .withData("chatId", message.chat.id)
+            .build()
+
+        val user = userService.getUser(fromUser.id)
 
         if (user != null) {
-            val actualUsername = message.from.usernameOrName
+            val actualUsername = fromUser.usernameOrName
 
             if (user.username != actualUsername) {
                 userService.updateUsername(user, actualUsername)
