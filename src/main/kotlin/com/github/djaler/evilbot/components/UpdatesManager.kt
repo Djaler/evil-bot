80b5f5673f482e6109e86a8192dbacb2afae2422
Kirill Romanov <djaler1@gmail.com>
2019-11-25 20:12:00 +0300

    improvement: добавлена запись breadcrumbs пройденных обработчиков событий

--- src/main/kotlin/com/github/djaler/evilbot/components/UpdatesManager.kt
+++ src/main/kotlin/com/github/djaler/evilbot/components/UpdatesManager.kt
@@ -2,6 +2,8 @@ package com.github.djaler.evilbot.components
 
 import com.github.djaler.evilbot.handlers.UpdateHandler
 import io.sentry.SentryClient
+import io.sentry.event.Breadcrumb
+import io.sentry.event.BreadcrumbBuilder
 import org.apache.logging.log4j.LogManager
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -21,7 +23,9 @@ class UpdatesManager(
         sentryClient.clearContext()
         for (handler in handlers) {
             try {
+                sentryClient.context.recordBreadcrumb(createHandlerBreadcrumb(handler, BreadcrumbCategory.CHECK))
                 if (handler.checkUpdate(update)) {
+                    sentryClient.context.recordBreadcrumb(createHandlerBreadcrumb(handler, BreadcrumbCategory.HANDLE))
                     val answered = handler.handleUpdate(update)
                     if (answered) {
                         break
@@ -31,10 +35,21 @@ class UpdatesManager(
             } catch (e: Exception) {
                 log.error("Handler: ${handler::class.simpleName}, update: $update", e)
 
-                sentryClient.context.addExtra("handler", handler::class.simpleName)
                 sentryClient.context.addExtra("update", update)
                 sentryClient.sendException(e)
             }
         }
     }
+}
+
+private enum class BreadcrumbCategory {
+    CHECK,
+    HANDLE
+}
+
+private fun createHandlerBreadcrumb(handler: UpdateHandler, category: BreadcrumbCategory): Breadcrumb {
+    return BreadcrumbBuilder().apply {
+        setMessage(handler::class.java.simpleName)
+        setCategory(category.name)
+    }.build()
 }
