ab6ff04bb2e92ea7b20902ff455b438d1604e52e
Kirill Romanov <djaler1@gmail.com>
2019-12-30 23:13:12 +0300

    refactor: UpdateHandler.checkUpdate совмещен с UpdateHandler.handleUpdate

--- src/main/kotlin/com/github/djaler/evilbot/handlers/UpdateHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/UpdateHandler.kt
@@ -2,9 +2,8 @@ package com.github.djaler.evilbot.handlers
 
 import org.telegram.telegrambots.meta.api.objects.Update
 
-interface UpdateHandler {
-    fun checkUpdate(update: Update): Boolean
 
+interface UpdateHandler {
     fun handleUpdate(update: Update): Boolean
 
     val order get() = 1
