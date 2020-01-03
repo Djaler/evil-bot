976f2de9a7df1fa7df2fdc6b76e88b578da50d21
Kirill Romanov <djaler1@gmail.com>
2020-01-03 15:18:57 +0300

    refactor: выделен CommonMessageHandler

--- src/main/kotlin/com/github/djaler/evilbot/handlers/MessageHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/MessageHandler.kt
@@ -1,6 +1,7 @@
 package com.github.djaler.evilbot.handlers
 
 import com.github.djaler.evilbot.filters.Filter
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.CommonMessageImpl
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.Message
 import com.github.insanusmokrassar.TelegramBotAPI.types.update.MessageUpdate
 import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.Update
@@ -19,4 +20,16 @@ abstract class MessageHandler(private val filter: Filter? = null) : UpdateHandle
     }
 
     protected abstract suspend fun handleMessage(message: Message): Boolean
+}
+
+abstract class CommonMessageHandler(private val filter: Filter? = null) : MessageHandler(filter) {
+    override suspend fun handleMessage(message: Message): Boolean {
+        if (message !is CommonMessageImpl<*>) {
+            return false
+        }
+
+        return handleMessage(message)
+    }
+
+    protected abstract suspend fun handleMessage(message: CommonMessageImpl<*>): Boolean
 }
