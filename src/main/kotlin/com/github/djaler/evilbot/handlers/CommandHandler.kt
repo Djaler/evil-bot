60cbed4024e06cd6800458d2e2291961677b01c3
Kirill Romanov <djaler1@gmail.com>
2020-01-06 19:41:52 +0300

    refactor: выделен CommandFilter

--- src/main/kotlin/com/github/djaler/evilbot/handlers/CommandHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/CommandHandler.kt
@@ -1,7 +1,8 @@
 package com.github.djaler.evilbot.handlers
 
+import com.github.djaler.evilbot.filters.CommandFilter
 import com.github.djaler.evilbot.filters.Filter
-import com.github.insanusmokrassar.TelegramBotAPI.types.MessageEntity.textsources.BotCommandTextSource
+import com.github.djaler.evilbot.filters.and
 import com.github.insanusmokrassar.TelegramBotAPI.types.User
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.CommonMessageImpl
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.TextContent
@@ -10,21 +11,11 @@ abstract class CommandHandler(
     private val botInfo: User,
     private val command: Array<String>,
     private val filter: Filter? = null
-) : CommonMessageHandler(filter) {
+) : CommonMessageHandler(
+    CommandFilter(command, botInfo).let { if (filter === null) it else it and filter }
+) {
     override suspend fun handleMessage(message: CommonMessageImpl<*>): Boolean {
         val content = message.content as? TextContent ?: return false
-
-        if (content.entities.none { it.source is BotCommandTextSource }) {
-            return false
-        }
-
-        val command = content.text.split(" ").first().drop(1).split("@")
-        if (command[0].toLowerCase() !in this.command) {
-            return false
-        }
-        if (command.size > 1 && "@${command[1].toLowerCase()}" != botInfo.username!!.username.toLowerCase()) {
-            return false
-        }
 
         val args = content.text.split(" ").drop(1)
 
