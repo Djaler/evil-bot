d739a174b724da8886b2f4d29282b7a0774fb1f7
Kirill Romanov <djaler1@gmail.com>
2020-01-14 20:20:49 +0300

    improvement: утилита getForm расширена для использования с Long

--- src/main/kotlin/com/github/djaler/evilbot/utils/NumberUtils.kt
+++ src/main/kotlin/com/github/djaler/evilbot/utils/NumberUtils.kt
@@ -1,10 +1,13 @@
 package com.github.djaler.evilbot.utils
 
-fun Int.getForm(firstForm: String, secondForm: String, thirdForm: String): String {
+fun Long.getForm(firstForm: String, secondForm: String, thirdForm: String): String {
     return when {
         this % 100 in 11..14 -> thirdForm
-        this % 10 == 1 -> firstForm
+        this % 10 == 1L -> firstForm
         this % 10 in 2..4 -> secondForm
         else -> thirdForm
     }
 }
+
+fun Int.getForm(firstForm: String, secondForm: String, thirdForm: String) =
+    toLong().getForm(firstForm, secondForm, thirdForm)
