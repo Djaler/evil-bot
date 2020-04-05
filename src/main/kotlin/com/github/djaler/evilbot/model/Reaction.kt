be8844e15ac8de8b8f50385af58241972e4766be
Kirill Romanov <djaler1@gmail.com>
2020-04-05 09:50:29 +0300

    chore: Reaction преобразован в data-класс

--- src/main/kotlin/com/github/djaler/evilbot/model/Reaction.kt
+++ src/main/kotlin/com/github/djaler/evilbot/model/Reaction.kt
@@ -2,13 +2,10 @@ package com.github.djaler.evilbot.model
 
 import javax.validation.constraints.NotEmpty
 
-class Reaction {
-    @NotEmpty
-    lateinit var reactions: List<String>
-
-    var triggers: List<String> = emptyList()
-
-    var chance = 100f
-
-    var replyToBot = false
-}
+data class Reaction(
+    @field:NotEmpty
+    val reactions: List<String>,
+    val triggers: List<String> = emptyList(),
+    val chance: Float = 100f,
+    val replyToBot: Boolean = false
+)
