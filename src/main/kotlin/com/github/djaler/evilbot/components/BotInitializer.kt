a8548e8a7ce96b2812f9c8dcf57fc18e25fbf6fb
Kirill Romanov <djaler1@gmail.com>
2020-04-23 22:03:18 +0300

    improvement: добавлена обработка ошибок парсинга обновлений

--- src/main/kotlin/com/github/djaler/evilbot/components/BotInitializer.kt
+++ src/main/kotlin/com/github/djaler/evilbot/components/BotInitializer.kt
@@ -6,7 +6,9 @@ import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.updates.startGettingOfUpdates
 import com.github.insanusmokrassar.TelegramBotAPI.utils.extensions.setWebhook
 import io.ktor.server.netty.Netty
+import io.sentry.SentryClient
 import kotlinx.coroutines.runBlocking
+import org.apache.logging.log4j.LogManager
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
@@ -14,8 +16,13 @@ import javax.annotation.PostConstruct
 class BotInitializer(
     private val requestExecutor: RequestsExecutor,
     private val telegramProperties: TelegramProperties,
-    private val updatesManager: UpdatesManager
+    private val updatesManager: UpdatesManager,
+    private val sentryClient: SentryClient
 ) {
+    companion object {
+        private val log = LogManager.getLogger()
+    }
+
     @PostConstruct
     fun init() {
         val webhook = telegramProperties.webhook
@@ -29,17 +36,24 @@ class BotInitializer(
                     listenRoute = path,
                     port = webhook.port,
                     engineFactory = Netty,
-                    allowedUpdates = updatesManager.getAllowedUpdates()
+                    allowedUpdates = updatesManager.getAllowedUpdates(),
+                    exceptionsHandler = { handleException(it) }
                 ) {
                     updatesManager.processUpdate(it)
                 }
             }
         } else {
             requestExecutor.startGettingOfUpdates(
-                allowedUpdates = updatesManager.getAllowedUpdates()
+                allowedUpdates = updatesManager.getAllowedUpdates(),
+                exceptionsHandler = { handleException(it) }
             ) {
                 updatesManager.processUpdate(it)
             }
         }
+    }
+
+    private suspend fun handleException(exception: Exception) {
+        log.error("Exception in update parsing", exception)
+        sentryClient.sendException(exception)
     }
 }
