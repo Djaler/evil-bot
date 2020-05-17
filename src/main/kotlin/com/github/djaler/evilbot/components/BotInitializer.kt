2f081248cea59eeb0d626f008d04548d60fafe32
Kirill Romanov <djaler1@gmail.com>
2020-05-17 13:43:41 +0300

    fix: исправлена настройка вебуха

--- src/main/kotlin/com/github/djaler/evilbot/components/BotInitializer.kt
+++ src/main/kotlin/com/github/djaler/evilbot/components/BotInitializer.kt
@@ -3,14 +3,25 @@ package com.github.djaler.evilbot.components
 import com.github.djaler.evilbot.config.TelegramProperties
 import com.github.djaler.evilbot.utils.getMD5
 import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.setWebhookInfoAndStartListenWebhooks
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.includeWebhookHandlingInRoute
 import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.startGettingOfUpdatesByLongPolling
 import com.github.insanusmokrassar.TelegramBotAPI.requests.webhook.SetWebhook
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.Update
+import com.github.insanusmokrassar.TelegramBotAPI.updateshandlers.UpdateReceiver
+import com.github.insanusmokrassar.TelegramBotAPI.utils.ExceptionHandler
+import io.ktor.routing.createRouteFromPath
+import io.ktor.routing.routing
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.sentry.SentryClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.runBlocking
 import org.apache.logging.log4j.LogManager
 import org.springframework.stereotype.Component
+import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
 
 @Component
@@ -32,7 +43,42 @@ class BotInitializer(
 
             runBlocking {
                 try {
-                    requestExecutor.setWebhookInfoAndStartListenWebhooks(
+                    requestExecutor.execute(
+                        SetWebhook(
+                            url = webhook.url + path,
+                            allowedUpdates = updatesManager.getAllowedUpdates()
+                        )
+                    )
+
+                    val listenRoute = path
+                    val listenHost = "0.0.0.0"
+                    val listenPort = webhook.port
+                    val scope = CoroutineScope(Executors.newFixedThreadPool(4).asCoroutineDispatcher())
+                    val exceptionsHandler: ExceptionHandler<Unit> = { handleException(it) }
+                    val block: UpdateReceiver<Update> = {
+                        updatesManager.processUpdate(it)
+                    }
+
+                    val env = applicationEngineEnvironment {
+                        module {
+                            routing {
+                                listenRoute.also {
+                                    createRouteFromPath(it).includeWebhookHandlingInRoute(
+                                        scope,
+                                        exceptionsHandler,
+                                        block
+                                    )
+                                }
+                            }
+                        }
+                        connector {
+                            host = listenHost
+                            port = listenPort
+                        }
+                    }
+                    embeddedServer(Netty, env).start(false)
+
+                    /*requestExecutor.setWebhookInfoAndStartListenWebhooks(
                         setWebhookRequest = SetWebhook(
                             url = webhook.url + path,
                             allowedUpdates = updatesManager.getAllowedUpdates()
@@ -44,7 +90,7 @@ class BotInitializer(
                         exceptionsHandler = { handleException(it) }
                     ) {
                         updatesManager.processUpdate(it)
-                    }
+                    }*/
                 } catch (e: Exception) {
                     log.error("Exception on webhook setup", e)
                     sentryClient.sendException(e)
