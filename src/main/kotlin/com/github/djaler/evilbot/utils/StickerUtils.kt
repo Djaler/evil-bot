0ef2a5a58086ca253f74f1dc51b9808459164674
Kirill Romanov <djaler1@gmail.com>
2020-10-04 09:45:28 +0300

    refactor: исправлено использование deprecated api

--- src/main/kotlin/com/github/djaler/evilbot/utils/StickerUtils.kt
+++ src/main/kotlin/com/github/djaler/evilbot/utils/StickerUtils.kt
@@ -1,7 +1,7 @@
 package com.github.djaler.evilbot.utils
 
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.formatting.link
 import com.github.insanusmokrassar.TelegramBotAPI.types.ParseMode.ParseMode
-import com.github.insanusmokrassar.TelegramBotAPI.utils.link
 
 fun createStickerpackLink(stickerPackName: String, parseMode: ParseMode): String {
     return (stickerPackName to "https://t.me/addstickers/$stickerPackName").link(parseMode)
