200f3ffef20068e759863655e39fa083cdc88326
Kirill Romanov <djaler1@gmail.com>
2020-10-19 00:15:45 +0300

    improvement: кэширование курсов валют

--- src/main/kotlin/com/github/djaler/evilbot/components/FixerClient.kt
+++ src/main/kotlin/com/github/djaler/evilbot/components/FixerClient.kt
@@ -3,6 +3,7 @@ package com.github.djaler.evilbot.components
 import com.github.djaler.evilbot.config.FixerApiProperties
 import io.ktor.client.*
 import io.ktor.client.request.*
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
@@ -12,6 +13,7 @@ class FixerClient(
     private val httpClient: HttpClient,
     private val fixerApiProperties: FixerApiProperties
 ) {
+    @Cacheable("fixerLatestRates")
     suspend fun getLatestRates(): Map<String, BigDecimal> {
         return httpClient.get<LatestRates>("http://data.fixer.io/api/latest") {
             parameter("access_key", fixerApiProperties.key)
