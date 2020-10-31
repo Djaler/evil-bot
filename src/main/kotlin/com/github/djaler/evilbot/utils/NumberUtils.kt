5b7ae4792afeec02bae33052a77fd841c90b690a
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2020-10-31 22:09:41 +0200

    feat: add roundToSignificantDigitsAfterComma helper extension method for BigDecimal type

--- src/main/kotlin/com/github/djaler/evilbot/utils/NumberUtils.kt
+++ src/main/kotlin/com/github/djaler/evilbot/utils/NumberUtils.kt
@@ -1,5 +1,8 @@
 package com.github.djaler.evilbot.utils
 
+import java.math.BigDecimal
+import java.math.MathContext
+
 fun Long.getForm(firstForm: String, secondForm: String, thirdForm: String): String {
     return when {
         this % 100 in 11..14 -> thirdForm
@@ -11,3 +14,8 @@ fun Long.getForm(firstForm: String, secondForm: String, thirdForm: String): Stri
 
 fun Int.getForm(firstForm: String, secondForm: String, thirdForm: String) =
     toLong().getForm(firstForm, secondForm, thirdForm)
+
+fun BigDecimal.roundToSignificantDigitsAfterComma(mc: MathContext): BigDecimal {
+    val fractionalPart = this.remainder(BigDecimal.ONE)
+    return this.subtract(fractionalPart).add(fractionalPart.round(mc))
+}
