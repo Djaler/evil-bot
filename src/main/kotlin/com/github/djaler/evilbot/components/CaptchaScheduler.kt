f558d3a4cefbc31645e8ece37912b2971ade014a
Kirill Romanov <djaler1@gmail.com>
2020-11-01 23:54:46 +0300

    feat: добавлена обработка ошибок в CaptchaScheduler

--- src/main/kotlin/com/github/djaler/evilbot/components/CaptchaScheduler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/components/CaptchaScheduler.kt
@@ -10,16 +10,24 @@ import dev.inmo.tgbotapi.extensions.utils.formatting.link
 import dev.inmo.tgbotapi.types.ParseMode.HTML
 import dev.inmo.tgbotapi.types.link
 import dev.inmo.tgbotapi.types.toChatId
+import io.sentry.SentryClient
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.apache.logging.log4j.LogManager
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class CaptchaScheduler(
     private val requestsExecutor: RequestsExecutor,
-    private val captchaService: CaptchaService
+    private val captchaService: CaptchaService,
+    private val exceptionsManager: ExceptionsManager,
+    private val sentryClient: SentryClient
 ) {
+    companion object {
+        private val log = LogManager.getLogger()
+    }
+
     private val parseMode = HTML
 
     @Scheduled(fixedRate = 1000 * 30)
@@ -28,23 +36,32 @@ class CaptchaScheduler(
 
         overdueRestrictions.forEach {
             GlobalScope.launch {
-                val chatId = it.chat.telegramId.toChatId()
-                val messageId = it.captchaMessageId
-                val userId = it.memberTelegramId.toUserId()
-                val cubeMessageId = it.cubeMessageId
+                try {
+                    val chatId = it.chat.telegramId.toChatId()
+                    val messageId = it.captchaMessageId
+                    val userId = it.memberTelegramId.toUserId()
+                    val cubeMessageId = it.cubeMessageId
 
-                requestsExecutor.editMessageText(
-                    chatId,
-                    messageId,
-                    "${("Ты" to userId.link).link(parseMode)} молчал слишком долго, прощай",
-                    parseMode
-                )
+                    requestsExecutor.editMessageText(
+                        chatId,
+                        messageId,
+                        "${("Ты" to userId.link).link(parseMode)} молчал слишком долго, прощай",
+                        parseMode
+                    )
 
-                requestsExecutor.kickChatMember(chatId, userId)
+                    requestsExecutor.kickChatMember(chatId, userId)
 
-                captchaService.removeRestriction(chatId, userId)
-                if (cubeMessageId != null) {
-                    requestsExecutor.deleteMessage(chatId, cubeMessageId)
+                    captchaService.removeRestriction(chatId, userId)
+                    if (cubeMessageId != null) {
+                        requestsExecutor.deleteMessage(chatId, cubeMessageId)
+                    }
+                } catch (e: Exception) {
+                    log.error("Restriction: $it", e)
+
+                    exceptionsManager.process(e)
+
+                    sentryClient.context.addExtra("restriction", it)
+                    sentryClient.sendException(e)
                 }
             }
         }
