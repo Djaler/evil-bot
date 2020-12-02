667c80358b529192b24c360931ba633bfadd504d
djaler <djaler1@gmail.com>
2020-12-02 23:20:07 +0300

    improvement: улучшен вывод времени для полдня

--- src/main/kotlin/com/github/djaler/evilbot/handlers/CurrentTimeHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/CurrentTimeHandler.kt
@@ -2,12 +2,15 @@ package com.github.djaler.evilbot.handlers
 
 import com.github.djaler.evilbot.service.TimeService
 import dev.inmo.tgbotapi.bot.RequestsExecutor
+import dev.inmo.tgbotapi.extensions.api.send.media.replyWithPhoto
 import dev.inmo.tgbotapi.extensions.api.send.reply
+import dev.inmo.tgbotapi.requests.abstracts.FileId
 import dev.inmo.tgbotapi.types.ExtendedBot
 import dev.inmo.tgbotapi.types.message.CommonMessageImpl
 import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
 import dev.inmo.tgbotapi.types.message.content.LocationContent
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Component
@@ -22,6 +25,8 @@ class CurrentTimeHandler(
 ) {
     companion object {
         private val dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        private val noonImage =
+            FileId("AgACAgIAAxkBAAIYYl_H9ZRoIRbKaPQreD_96Tz-M5jqAAIjsDEbG1VBSt5s1ejS7mc0fbvRly4AAwEAAwIAA3kAAyevAwABHgQ")
     }
 
     override suspend fun handleCommand(message: CommonMessageImpl<*>, args: String?) {
@@ -45,6 +50,11 @@ class CurrentTimeHandler(
             return
         }
 
+        if (isNoon(timeForLocation)) {
+            requestsExecutor.replyWithPhoto(message, noonImage)
+            return
+        }
+
         val formattedLocationTime = timeForLocation.format(dateTimeFormatter)
 
         requestsExecutor.reply(
@@ -56,4 +66,6 @@ class CurrentTimeHandler(
             }
         )
     }
+
+    private fun isNoon(timeForLocation: LocalDateTime) = timeForLocation.hour == 12 && timeForLocation.minute == 0
 }
