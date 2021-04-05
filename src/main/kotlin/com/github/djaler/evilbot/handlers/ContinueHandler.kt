4f22a775aac6e6e3e25bee74c093a4bcba4cf48d
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2021-04-05 13:22:23 +0300

    improvement: ContinueHandler отвечает на сообщение с обрабатываемым текстом

--- src/main/kotlin/com/github/djaler/evilbot/handlers/ContinueHandler.kt
+++ src/main/kotlin/com/github/djaler/evilbot/handlers/ContinueHandler.kt
@@ -8,6 +8,7 @@ import dev.inmo.tgbotapi.extensions.utils.asTextContent
 import dev.inmo.tgbotapi.types.ExtendedBot
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 import dev.inmo.tgbotapi.types.message.abstracts.FromUserMessage
+import dev.inmo.tgbotapi.types.message.abstracts.Message
 import dev.inmo.tgbotapi.types.message.content.TextContent
 import dev.inmo.tgbotapi.types.textLength
 import org.springframework.stereotype.Component
@@ -26,21 +27,27 @@ class ContinueHandler(
         message: M,
         args: String?
     ) where M : CommonMessage<TextContent>, M : FromUserMessage {
-        val replyToText = message.replyTo?.asContentMessage()?.content?.asTextContent()?.text
+        var messageToReply: Message = message
+        var sourceText = args
 
-        val sourceText = args ?: replyToText
+        val replyTo = message.replyTo
+        val replyMessageText = replyTo?.asContentMessage()?.content?.asTextContent()?.text
+        if (replyMessageText !== null) {
+            messageToReply = replyTo
+            sourceText = replyMessageText
+        }
 
-        if (sourceText === null || (args !== null && replyToText !== null)) {
-            requestsExecutor.reply(message, "Либо пришли текст, либо ответь командой на текстовое сообщение")
+        if (sourceText === null) {
+            requestsExecutor.reply(messageToReply, "Либо пришли текст, либо ответь командой на текстовое сообщение")
             return
         }
 
         try {
             val prediction = predictionService.getPrediction(sourceText, leaveSource = false)
 
-            prediction.chunked(textLength.last).forEach { requestsExecutor.reply(message, it) }
+            prediction.chunked(textLength.last).forEach { requestsExecutor.reply(messageToReply, it) }
         } catch (e: Exception) {
-            requestsExecutor.reply(message, "Не получилось, попробуй ещё")
+            requestsExecutor.reply(messageToReply, "Не получилось, попробуй ещё")
         }
     }
 }
