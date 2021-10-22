b5dd276e1a7f1c2c8930ab611ae3e8226448a696
Kirill Romanov <djaler1@gmail.com>
2021-10-22 21:58:41 +0300

    feat: хранение идентификатора пользователя Telegram как BIGINT вместо INT

--- src/main/kotlin/com/github/djaler/evilbot/repository/UserRepository.kt
+++ src/main/kotlin/com/github/djaler/evilbot/repository/UserRepository.kt
@@ -4,5 +4,5 @@ import com.github.djaler.evilbot.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Int> {
-    fun findByTelegramId(telegramId: Int): User?
+    fun findByTelegramId(telegramId: Long): User?
 }
