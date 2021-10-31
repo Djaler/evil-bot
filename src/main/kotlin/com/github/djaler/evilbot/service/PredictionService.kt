f1e72f7c3f28519b4dcb002603643e021b95caa4
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2021-11-01 01:36:05 +0300

    chore: удалено исключение TooBigTextException, так как оно выбрасывалось даже если проблема была не в размере текста

--- src/main/kotlin/com/github/djaler/evilbot/service/PredictionService.kt
+++ src/main/kotlin/com/github/djaler/evilbot/service/PredictionService.kt
@@ -7,16 +7,8 @@ import org.springframework.stereotype.Service
 class PredictionService(
     private val sberClient: SberClient
 ) {
-    class TooBigTextException : Exception()
-
-    @Throws(TooBigTextException::class)
     suspend fun getPrediction(text: String, leaveSource: Boolean = true): String {
         val prediction = sberClient.getPrediction(text)
-
-        if (prediction.length <= text.length) {
-            throw TooBigTextException()
-        }
-
         return if (leaveSource) prediction else prediction.removePrefix(text)
     }
 }
