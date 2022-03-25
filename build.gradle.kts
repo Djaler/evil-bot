9d54d2c8fa16a27820ff113cb2d646b2e9f6a42a
Kirill Romanov <djaler1@gmail.com>
2022-03-25 20:58:18 +0300

    build(deps): поднята версия kotlin до 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.5.20"
+    val kotlinVersion = "1.6.10"
 
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
