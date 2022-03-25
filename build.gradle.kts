5319db790d98fb2c8b260f1a73a540c3c150a731
Kirill Romanov <djaler1@gmail.com>
2022-03-25 20:58:59 +0300

    deps: поднята версия ktor до 1.6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,9 +43,9 @@ dependencies {
 
     implementation("dev.inmo:tgbotapi:0.38.10")
 
-    implementation("io.ktor:ktor-server-netty:1.6.0")
-    implementation("io.ktor:ktor-client-apache:1.6.0")
-    implementation("io.ktor:ktor-client-jackson:1.6.0")
+    implementation("io.ktor:ktor-server-netty:1.6.8")
+    implementation("io.ktor:ktor-client-apache:1.6.8")
+    implementation("io.ktor:ktor-client-jackson:1.6.8")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
