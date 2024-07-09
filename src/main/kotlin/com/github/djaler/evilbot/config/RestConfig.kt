62acbf1e890e1ba0e53ec6103747ac8c7396b334
Kirill Romanov <djaler1@gmail.com>
2024-07-09 20:12:22 +0300

    fix: реализован Sentry плагин для ktor client

--- src/main/kotlin/com/github/djaler/evilbot/config/RestConfig.kt
+++ src/main/kotlin/com/github/djaler/evilbot/config/RestConfig.kt
@@ -1,12 +1,19 @@
 package com.github.djaler.evilbot.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.ktor.client.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.http.*
-import io.ktor.serialization.jackson.*
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.api.createClientPlugin
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.request
+import io.ktor.http.ContentType
+import io.ktor.serialization.jackson.JacksonConverter
+import io.sentry.Breadcrumb
+import io.sentry.Sentry
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -24,6 +31,34 @@ class RestConfig {
             install(Logging) {
                 level = LogLevel.ALL
             }
+
+            install(SentryPlugin)
         }
     }
+}
+
+val SentryPlugin = createClientPlugin("SentryPlugin") {
+    onRequest { request, content ->
+        Sentry.addBreadcrumb(
+            Breadcrumb.http(
+                request.url.toString(),
+                request.method.value
+            ).apply {
+                setData("content", content)
+            }
+        )
+    }
+
+    val responseObserver = ResponseObserver({ response ->
+        val request = response.request
+        Sentry.addBreadcrumb(
+            Breadcrumb.http(
+                request.url.toString(),
+                request.method.value
+            ).apply {
+                setData("content", response.bodyAsText())
+            }
+        )
+    })
+    ResponseObserver.install(responseObserver, client)
 }
