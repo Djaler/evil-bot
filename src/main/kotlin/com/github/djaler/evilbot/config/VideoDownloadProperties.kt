19c410edce6135ea56035e55d88acf59885652d6
PatakIN <git@patakin.space>
2025-09-17 22:44:11 +0500

    fix: Скачивание медиа-контента через yt-dlp с указанием доменов

--- src/main/kotlin/com/github/djaler/evilbot/config/VideoDownloadProperties.kt
+++ src/main/kotlin/com/github/djaler/evilbot/config/VideoDownloadProperties.kt
@@ -5,8 +5,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties("video.download")
 class VideoDownloadProperties {
     /**
+     * Включает/выключает фичу скачивания видео по ссылкам.
+     * Если null — считаем выключенной, пока не переопределено в конфиге.
+     */
+    var enabled: Boolean? = null
+
+    /**
+     * Разрешенные домены, с которых можно скачивать видео.
+     * Поддомены также считаются валидными (например, www.youtube.com).
+     */
+    lateinit var allowedDomains: List<String>
+
+    /**
      * Максимальный размер загружаемого файла yt-dlp.
      * Формат как у yt-dlp: например 50M, 100M, 1G.
      */
-    var maxFilesize: String = "50M"
+    lateinit var maxFilesize: String
 }
